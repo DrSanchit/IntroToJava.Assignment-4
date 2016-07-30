12ee2748e1e43f72132d70aae994b20cddccafb7
DrSanchit <sanchit.bhatnagar@hotmail.com>
2016-07-30 13:22:51 +0530

    Assignmnet-1.4
    Assume that the integer variable x is 5 and the integer variable y is
    10. Give the values of the following
    expressions:
    a. x + y * 2
    b. x - y + 2
    c. (x+y) * 2
    d. y % x

---
+++ Expressions.java
@@ -0,0 +1,17 @@
+public class Expressions {
+	public static void main(String[] args) {
+		
+		int x = 5,y=10;
+		
+			int a=x+y*2;
+			int b = x-y+2;
+			int c=(x+y)*2;
+			int d=y%x;
+			
+			System.out.println(a);
+			System.out.println(b);
+			System.out.println(c);
+			System.out.println(d);
+	}
+
+}
